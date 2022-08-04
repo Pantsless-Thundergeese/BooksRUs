fd78f74976c3e3f51e2a6872767ea7c161376491
Alyssa Chew <alyssachew@MacBook-Pro-2.local>
2022-08-04 14:20:11 -0700

    removal from cart should now work

--- client/store.js
+++ client/store.js
@@ -1,5 +1,26 @@
 import { createStore, action } from 'easy-peasy';
 
+function isEqual(obj1, obj2) {
+  var props1 = Object.getOwnPropertyNames(obj1);
+  var props2 = Object.getOwnPropertyNames(obj2);
+  if (props1.length != props2.length) {
+      return false;
+  }
+  for (var i = 0; i < props1.length; i++) {
+      let val1 = obj1[props1[i]];
+      let val2 = obj2[props1[i]];
+      let isObjects = isObject(val1) && isObject(val2);
+      if (isObjects && !isEqual(val1, val2) || !isObjects && val1 !== val2) {
+          return false;
+      }
+  }
+  return true;
+}
+
+function isObject(object) {
+  return object != null && typeof object === 'object';
+}
+
 const store = createStore({
   user: {
     likedBooks: []
@@ -31,12 +52,19 @@ const store = createStore({
       ...state.cart,
       payload
     ]
+    console.log("cart after add: ", state.cart)
   }),
   removeFromCart: action((state, payload) => {
-    const index = state.cart.findIndex((elem) => (elem === payload));
     const cart = state.cart.slice();
-    const removed = cart.splice(index - 1, 1);
+    console.log("cart before removal: ", cart)
+    console.log("payload to remove: ", payload)
+    const index = cart.findIndex((elem) =>
+      isEqual(elem, payload)
+    );
+    console.log(index);
+    const removed = cart.splice(index, 1);
     console.log("removed: ", removed)
+    console.log("cart after removal: ", cart)
     state.cart = cart;
   })
 }, // model
